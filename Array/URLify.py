f5571f656d74e9654ff4c6902a115b671a4edb38
two-for-five <lyf19931231@gmail.com>
2017-12-18 18:55:39 -0500

    URLify

--- Array/URLify.py
+++ Array/URLify.py
@@ -1,4 +1,24 @@
 import unittest
 
-def URLify():
-    pass
+def URLify(string):
+    """
+    replace every space to %20
+    :param string:
+    :return: new_string
+    """
+    string = string.strip()
+    string = string.split(" ")
+    return "%20".join(string)
+
+class Test(unittest.TestCase):
+    data = [
+        ('much ado about nothing      ',
+         'much%20ado%20about%20nothing'),
+        ('Mr John Smith    ','Mr%20John%20Smith')]
+    def test(self):
+        for old_str, expected in self.data:
+            actual = URLify(old_str)
+            self.assertEqual(actual, expected)
+
+if __name__ == "main":
+    unittest.main()
