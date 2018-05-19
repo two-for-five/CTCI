91e1376e98edca17f9696d947cc6d77fe1578979
Yifeng Liu <yifeng.liu@duke.edu>
2018-05-18 21:14:47 -0400

    update

--- interview/practice.js
+++ interview/practice.js
@@ -1,7 +1,5 @@
-function sum(x){
-    return function(y){
-      return x + y;
-    }
-
-}
-console.log(sum(1));
+var a = 1;
+var b = [1,2,4];
+var c = b.map((num) =>{
+  console.log(this.a);
+}.bind(this))
